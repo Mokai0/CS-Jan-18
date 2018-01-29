0cd91b5323a37533e7c06a2a22e2bc1d8b7bf698
Mohammad Hanbali <mokai86@gmail.com>
2018-01-29 11:47:17 -0500

    Created the variables for invoice objects as well as built a simple constructor. Omitted Expirey date for now because not sure how to handle date and time variables.

--- testapp/testapp/Invoice.cs
+++ testapp/testapp/Invoice.cs
@@ -8,5 +8,27 @@ namespace testapp
 {
     class Invoice
     {
+        public string   Brand;
+        public string   Product;
+        public int      Size;
+        //These three variables will be combined to construct the name of the item.
+        public int      Recieved;
+        //This variable will represent the quantity of said item that has been recieved, "how many cases".
+        public int      Cost;
+        //Cost per case.
+        //public DateTime ExpDate;
+        //When the new product will expire - this variable object might give me trouble later so I'll omit it for now.
+        public double   MarkUp;
+        //This should be the 1 + a percentage, 1.39 is a 39% mark up.
+
+        Invoice(string brand, string product, int size, int recieved, int cost, double markup)
+        {
+            Brand = brand;
+            Product = product;
+            Size = size;
+            Recieved = recieved;
+            Cost = cost;
+            MarkUp = markup;
+        }
     }
 }
