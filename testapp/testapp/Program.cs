54194069b5fcf7d5a5be15e8b641d6d5481b8447
Mohammad Hanbali <mokai86@gmail.com>
2018-01-29 11:14:45 -0500

    Added some classes and some example options to be fleshed out later

--- testapp/testapp/Program.cs
+++ testapp/testapp/Program.cs
@@ -11,11 +11,32 @@ namespace testapp
 		static void Main(string[] args)
 		{
 			Console.WriteLine("What do you want to do?");
-			var Menu = "See current Stock \nCreate a new Invoice" + Environment.NewLine + "Check invoice History";
+
+			var Menu = "1] See current Stock \n2] Create a new Invoice" + Environment.NewLine + "3] Check invoice History";
 			//In the above line of code "\n" and "Environment.NewLine" serve the same function however while \n is much shorter Environment.NewLine is supposedly more context correct/savvy. 
 
 			Console.WriteLine(Menu);
-			Console.ReadKey();
+
+            string selection = Console.ReadLine();
+            if ( selection == "1")
+            {
+                Console.WriteLine("You've selected to see your current Stock.");
+            }
+            else if( selection == "2")
+            {
+                Console.WriteLine("You've selected to create a new Invoice.");
+            }
+            else if( selection == "3")
+            {
+                Console.WriteLine("You've selected to see your invoice History.");
+            }
+            else
+            {
+                Console.WriteLine("You didn't pick a valid option");
+            }
+
+
+            Console.ReadKey();
 		}
 	}
 }
