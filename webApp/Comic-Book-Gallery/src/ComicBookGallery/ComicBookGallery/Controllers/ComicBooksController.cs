9f62f69cf2df85205fdc8c997f5d241db040d3b2
MoKai <mokai86@gmail.com>
2018-02-04 22:10:21 -0500

    Created a ComicBooks list view

--- webApp/Comic-Book-Gallery/src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ webApp/Comic-Book-Gallery/src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -17,6 +17,13 @@ namespace ComicBookGallery.Controllers
             _comicBookRepository = new ComicBookRepository();
         }
 
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBook();
+
+            return View(comicBooks);
+        }
+
         public ActionResult Detail(int? id) //The ? here makes the id parameter 'nullable'
         {
             if (id == null)
