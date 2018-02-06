8889fcefc6b58b8aa40a71c7bf79cdacc7f5ade0
MoKai <mokai86@gmail.com>
2018-02-05 20:23:58 -0500

    Added entries controller AddPost Method

--- webApp/aspnet-fitness-frog/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ webApp/aspnet-fitness-frog/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -44,6 +44,12 @@ namespace Treehouse.FitnessFrog.Controllers
             return View();
         }
 
+        [ActionName("Add"), HttpPost] //This is called an 'attribute' and it allows you to associate multiple methods to the same method call
+        public ActionResult AddPost()
+        {
+            return View();
+        }
+
         public ActionResult Edit(int? id)
         {
             if (id == null)
