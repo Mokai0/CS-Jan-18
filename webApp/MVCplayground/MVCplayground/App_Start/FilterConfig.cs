4ceb7b571c4b63bfc72cbf9872b7841e556479ab
MoKai <mokai86@gmail.com>
2018-02-07 21:53:08 -0500

    Started up a new project to test out some things. Started to use 'Browser Reload on Save' extension - might prove to be extra useful.

---
+++ webApp/MVCplayground/MVCplayground/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCplayground
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ webApp/MVCplayground/MVCplayground/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCplayground.MvcApplication" Language="C#" %>
