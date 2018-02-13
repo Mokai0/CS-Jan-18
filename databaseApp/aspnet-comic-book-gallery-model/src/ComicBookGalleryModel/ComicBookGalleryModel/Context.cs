327c1bbcdbc8c3764444daffea487ae6d7f4e020
MoKai <mokai86@gmail.com>
2018-02-12 22:51:32 -0500

    Just built a basic app that deals with local sql databases using Entity Frameworks. Learned different ways to set configurations and naming for the database. Also learned how to setup a database schema using the 'Code First' EF workflow.

---
+++ databaseApp/aspnet-comic-book-gallery-model/src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -0,0 +1,26 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    public class Context : DbContext
+    {
+        //Method #1 for renaming and instantiating a db
+        //public  Context() : base("ComicBookGallery") {}
+
+        //Method #2 
+        //public Context() : base("Data Source=(localdb)" +
+        //    @"\MSSQLLocalDB;Initial Catalog=ComicBookGallery;" +
+        //    //Note the '@' this tells the compiler to interperet the next line of code as a string literal instead of a string. This is needed here because '\' is used to break the following text out of the string format. Another method to remedy this would be to add a second '\' which would tell the compiler to not read this '\' as a break in the string, like this:
+        //    //"\\MSSQLLocalDB;Initial Catalog=ComicBookGallery;"
+        //    "Integrated Security=True;MultipleActiveResultSets=True")
+        //{ }
+
+        public DbSet<ComicBook> ComicBooks { get; set; }
+    }
+}
