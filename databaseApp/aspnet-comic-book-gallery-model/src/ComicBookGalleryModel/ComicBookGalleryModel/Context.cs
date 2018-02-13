a4601e49ae6413d16194b6bf406c3021b45f6b75
MoKai <mokai86@gmail.com>
2018-02-12 23:07:58 -0500

    When implementing a Code First EF and editing the Model you need to set the db initializer to drop the old db and create a new one, this can be done two different ways. Either whenever the model changes or always - as in every time the app is launched.

--- databaseApp/aspnet-comic-book-gallery-model/src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
+++ databaseApp/aspnet-comic-book-gallery-model/src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -10,16 +10,12 @@ namespace ComicBookGalleryModel
 {
     public class Context : DbContext
     {
-        //Method #1 for renaming and instantiating a db
-        //public  Context() : base("ComicBookGallery") {}
-
-        //Method #2 
-        //public Context() : base("Data Source=(localdb)" +
-        //    @"\MSSQLLocalDB;Initial Catalog=ComicBookGallery;" +
-        //    //Note the '@' this tells the compiler to interperet the next line of code as a string literal instead of a string. This is needed here because '\' is used to break the following text out of the string format. Another method to remedy this would be to add a second '\' which would tell the compiler to not read this '\' as a break in the string, like this:
-        //    //"\\MSSQLLocalDB;Initial Catalog=ComicBookGallery;"
-        //    "Integrated Security=True;MultipleActiveResultSets=True")
-        //{ }
+        public Context()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
+            //Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+        }
 
         public DbSet<ComicBook> ComicBooks { get; set; }
     }
--- databaseApp/aspnet-comic-book-gallery-model/src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
+++ databaseApp/aspnet-comic-book-gallery-model/src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
@@ -9,12 +9,10 @@ namespace ComicBookGalleryModel.Models
     public class ComicBook
     {
         public int Id { get; set; }
-        //If the class contains a property named Id, a primary key column with the same name and data type will be added to the entity's database table. Because we're using a numeric data type, EF will also make the column an identity column. Identity columns are automatically assigned values by the database when new records are added to the table. Primary key properties, can also be named:
-        //"ID" "ClassNameId" or "ClassNameID"
-
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string Description { get; set; }
         public DateTime PublishedOn { get; set; }
+        public double? AverageRating { get; set; }
     }
 }
