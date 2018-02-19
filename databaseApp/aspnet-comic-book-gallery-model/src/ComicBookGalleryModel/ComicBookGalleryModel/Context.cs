5fcd6268248a240af0416f61a68747c773e46e8b
MoKai <mokai86@gmail.com>
2018-02-18 20:00:07 -0500

    'How-To' make a custom Database Initializer to improve seeding

--- databaseApp/aspnet-comic-book-gallery-model/src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
+++ databaseApp/aspnet-comic-book-gallery-model/src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -15,7 +15,9 @@ namespace ComicBookGalleryModel
         {
             //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
             //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
-            Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            //Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+
+            Database.SetInitializer(new DatabaseInitializer());
         }
 
         public DbSet<ComicBook> ComicBooks { get; set; }
@@ -24,14 +26,9 @@ namespace ComicBookGalleryModel
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
 
-            //modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
-            //modelBuilder.Conventions.Add(new DecimalPropertyConvention(5, 2));
-            //[SHOTGUN] While the above method works in restructuring the AvgRating property's decimal format it also reformats the decimal formatting for every decimal instance. This doesn't cause any problems for the app right now but it could allow some bugs later if we end up using another decimal property.
-
             modelBuilder.Entity<ComicBook>()
                 .Property(cb => cb.AverageRating)
                 .HasPrecision(5, 2);
-            //[SNIPER] This method will specifically target the average ratings of each comic book and nothing else.
         }
     }
 }
