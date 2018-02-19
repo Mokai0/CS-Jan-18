7b491d73f80054714a0b590fd7133e7b7d79ddac
MoKai <mokai86@gmail.com>
2018-02-18 19:13:51 -0500

    Reassessing null values/columns and restricting character counts. Also some asthetic schema work (commented out).

--- databaseApp/aspnet-comic-book-gallery-model/src/ComicBookGalleryModel/ComicBookGalleryModel/Models/Artist.cs
+++ databaseApp/aspnet-comic-book-gallery-model/src/ComicBookGalleryModel/ComicBookGalleryModel/Models/Artist.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ComicBookGalleryModel.Models
 {
+    //[Table("Talent")]
+    //This will rename the Artist table asthetically
     public class Artist
     {
         public Artist()
@@ -14,6 +18,9 @@ namespace ComicBookGalleryModel.Models
         }
 
         public int Id { get; set; }
+        [Required, StringLength(50)]
+        //[Column("FullName")]
+        //This will rename the Artist's Name Column asthetically
         public string Name { get; set; }
 
         public ICollection<ComicBookArtist> ComicBooks { get; set; }
--- databaseApp/aspnet-comic-book-gallery-model/src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBookArtist.cs
+++ databaseApp/aspnet-comic-book-gallery-model/src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBookArtist.cs
@@ -10,13 +10,9 @@ namespace ComicBookGalleryModel.Models
     {
         public int Id { get; set; }
         public int ComicBookId { get; set; }
-        //This is a foreign key property to the comicbook that we're adding artists to
         public int ArtistId { get; set; }
-        //This is a foreign key property to each artist that will be added to the comic book
         public int RoleId { get; set; }
-        //Belongs to artist's role, also a foreign key property
 
-        //Naviigation properties
         public ComicBook ComicBook { get; set; }
         public Artist Artist { get; set; }
         public Role Role { get; set; }
