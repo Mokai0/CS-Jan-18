0972259da85076d8c6abb396bf54c3ddb57b7533
MoKai <mokai86@gmail.com>
2018-03-12 22:03:39 -0400

    Started working on a View

--- webApp/MVCtake2/src/MVCtake2/Controllers/HomePageController.cs
+++ webApp/MVCtake2/src/MVCtake2/Controllers/HomePageController.cs
@@ -10,12 +10,7 @@ namespace MVCtake2.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("It's the Home Page!");
+            return View();
         }
     }
 }
