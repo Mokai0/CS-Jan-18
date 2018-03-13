b5e5d22d206724df8621b3662645c6766ede3f51
MoKai <mokai86@gmail.com>
2018-03-12 21:54:16 -0400

    Started wiring up controller class

---
+++ webApp/MVCtake2/src/MVCtake2/Controllers/HomePageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCtake2.Controllers
+{
+    public class HomePageController : Controller
+    {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return new RedirectResult("/");
+            }
+
+            return new ContentResult()
+            {
+                Content = "It's the Home Page!"
+            };
+        }
+    }
+}
