c4707434bb9c656ef929b8d572d06502189e1f9c
MoKai <mokai86@gmail.com>
2018-03-13 00:06:20 -0400

    Added a Repository

---
+++ webApp/MVCtake2/src/MVCtake2/Data/GameRepo.cs
@@ -0,0 +1,44 @@
+﻿using MVCtake2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCtake2.Data
+{
+    public class GameRepo
+    {
+        private static Game[] _games = new Game[]
+        {
+            new Game()
+            {
+                Title = "Super Smash Bros",
+                Genre = "Fighting",
+                Description = "<p>A high octane brawler!</p>",
+                Developers = new string[]
+                {"Nintendo", "The Kirby Guy"},
+                Favorite = false
+            },
+            new Game()
+            {
+                Title = "Super Smash Bros Melee",
+                Genre = "Fighting",
+                Description = "<p>The stupifying experience continues!</p>",
+                Developers = new string[]
+                {"Nintendo", "The Kirby Guy"},
+                Favorite = true
+            }
+    };
+
+        public Game GetGame(int id)
+        {
+            Game gameToReturn = null;
+            foreach (var game in _games)
+            {
+                gameToReturn = game;
+                break;
+            }
+            return gameToReturn;
+        }
+    }
+}
