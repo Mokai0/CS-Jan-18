366de970785dd7c6fe876b3230b7849000cc8fad
MoKai <mokai86@gmail.com>
2018-03-18 14:09:16 -0400

    Began building the Repository class, initiated the context

---
+++ testBin/InventoryAppMock1/InventoryShared/Data/Repository.cs
@@ -0,0 +1,24 @@
+﻿using InventoryShared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace InventoryShared.Data
+{
+    //This class should handle the various CRUD operations handling
+    public static class Repository
+    {
+        //<summary> Private method that returns a db context to the class
+        //<returns> An instance of the context class
+        static Context GetContext()
+        {
+            var context = new Context();
+            context.Database.Log = (message) => Debug.WriteLine(message);
+            return context;
+        }
+    }
+}
