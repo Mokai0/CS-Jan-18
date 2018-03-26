5e65d22f8f6a4cbbea51c1a78511eaac88ca8926
MoKai <mokai86@gmail.com>
2018-03-26 16:40:50 -0400

    Constructed the tests and all is red, now to make them green

--- unitTesting/TreehouseDefense/TreehouseDefenseTests/PathTests.cs
+++ unitTesting/TreehouseDefense/TreehouseDefenseTests/PathTests.cs
@@ -4,21 +4,37 @@ namespace TreehouseDefense.Tests
 {
     public class PathTests
     {
-        [Fact]
-        public void MapLoactionIsOnPath()
-        {
-            var map = new Map(3, 3);
+        private Map _map3x3;
+        private MapLocation[] _pathLocations3;
+        private Path _path3;
 
-            MapLocation[] pathLocations =
+        public PathTests()
+        {
+            _map3x3 = new Map(3, 3);
+
+            _pathLocations3 = new MapLocation[]
             {
-                new MapLocation(0,1,map),
-                new MapLocation(1,1,map),
-                new MapLocation(2,1,map)
+                new MapLocation(0,1,_map3x3),
+                new MapLocation(1,1,_map3x3),
+                new MapLocation(2,1,_map3x3)
             };
 
-            var target = new Path(pathLocations);
+            _path3 = new Path(_pathLocations3);
+        }
 
-            Assert.True(target.IsOnPath(new MapLocation(0, 1, map)));
+        [Fact]
+        public void MapLoactionIs_OnPath()
+        {
+            var target = _path3;
+            Assert.True(target.IsOnPath(new MapLocation(0, 1, _map3x3)));
+        }
+
+        [Fact]
+        public void MapLoactionIs_NotOnPath()
+        {
+            var target = _path3;
+
+            Assert.True(target.IsOnPath(new MapLocation(0, 0, _map3x3)));
         }
     }
 }
